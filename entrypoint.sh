8a0ebf590d0574426ee08ec12bee9c8391389f58
rith-me <gbol7415@gmail.com>
2025-04-11 13:51:52 +0700

    update dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Laravel setup
-php artisan config:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan storage:link
-php artisan migrate --force
+cd /var/www
 
-# Start Supervisor
+# Run Laravel setup if needed
+composer install --no-dev --optimize-autoloader
+
+php artisan config:cache
+php artisan migrate --force
+php artisan storage:link
+
+# Start the Laravel dev server
+php artisan serve --host=0.0.0.0 --port=8000
